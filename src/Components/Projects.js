8838e501e4f870f43fc50cd0deb045799245cd02
jamwil123 <james789111@gmail.com>
2021-12-24 23:18:53 +0000

    fixed bugs

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,8 +2,6 @@ import React, { useRef } from "react";
 import ProjectCards from "../Components/ProjectCards";
 
 export default function Projects() {
-  const myRef = useRef(null);
-  const executeScroll = () => myRef.current.scrollIntoView();
   let tag = "</>";
   return (
     <main className="projectsMain">
