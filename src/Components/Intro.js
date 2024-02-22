3b5d175490f9992414c2ac25831b6f96a10a711c
James <james789111@gmail.com>
2024-02-22 23:28:33 +0700

    Adding new skill

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,6 +1,5 @@
 import React from "react";
 import jamesFace from "../IMG_6790.jpg";
-import {Img} from "react-image-loading"
 
 export default function Intro() {
   return (
@@ -9,20 +8,20 @@ export default function Intro() {
         <div className="introText">
           <main className="introTextMain">
             <div className="nameAndWelcome">
-              <p className='hiJames'>Hi, I'm James. </p>
+              <p className="hiJames">Hi, Im James. </p>
               <p> I am a Junior Developer!</p>
             </div>
             <p className="introSubText">
               <p>Please take a look around my website,</p>
 
               <p>you can see my past projects and details to contact me</p>
-              </p>
-              </main>
-              </div>
-              <div className="homeImg">
-              <Img src={jamesFace} alt="james" className="jamesPicture" />
-              </div>
-              </div>
-          </div>
+            </p>
+          </main>
+        </div>
+        <div className="homeImg">
+          <img src={jamesFace} alt="james" className="jamesPicture" />
+        </div>
+      </div>
+    </div>
   );
 }
